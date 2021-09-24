20a6096be0a61ec530a3e4c8b40079e5beecfa1c
KapilKantPal <kapilkantpal@gmail.com>
2021-09-24 13:51:34 +0530

    Re-Committing master branch

--- app/src/main/java/com/programmingmanav/gitbasic/MainActivity.java
+++ app/src/main/java/com/programmingmanav/gitbasic/MainActivity.java
@@ -3,21 +3,30 @@ package com.programmingmanav.gitbasic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     int num;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        num = (int)( Math.random()*(101));
+        text = (TextView)findViewById(R.id.textView);
 
     }
 
     public void genNumber(View view) {
+        num = (int)( Math.random()*(101));
+        Log.i("Random" , String.valueOf(num));
+        text.setText(" Random Number = "+num);
+    }
+
+    public void Next(View view) {
     }
 }
